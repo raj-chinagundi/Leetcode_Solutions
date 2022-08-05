e2874777f33e5899cce43058ab86de6924bbcff1
Basavraj Chinagundi <75333151+basavraj-chinagundi@users.noreply.github.com>
2022-08-05 14:53:58 +0530

    Time: 876 ms (5.78%), Space: 331.6 MB (5.17%) - LeetHub

---
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -0,0 +1,51 @@
+class MyHashMap {
+public:
+    vector<list<pair<int,int>>> m;
+    int siz;
+    MyHashMap() {
+        siz=1e6+1;
+        m.resize(siz);    
+    }
+    
+    int hash(int key){
+        return key%siz;
+    }
+    list<pair<int,int>> :: iterator search(int key){
+        int i=hash(key);
+        //pair h toh uska first element access krna h muje
+        list<pair<int,int>> :: iterator it=m[i].begin();
+        while(it!=m[i].end()){
+            if(it->first==key){
+                return it;
+            }
+        }
+        return it;
+    }
+    
+    void put(int key, int value) {
+        int i=hash(key);
+        remove(key);
+        m[i].push_back({key,value});
+    }
+    
+    int get(int key) {
+        int i=hash(key);
+        list<pair<int,int>> :: iterator it=search(key);
+        if(it==m[i].end())return -1;
+        else return it->second;
+    }
+    
+    void remove(int key) {
+        int i=hash(key);
+        list<pair<int,int>> :: iterator it=search(key);
+        if(it!=m[i].end())m[i].erase(search(key));
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
