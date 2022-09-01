7a1dbc33843a3d76af5117500e891af01a90b6c1
Basavraj Chinagundi <75333151+basavraj-chinagundi@users.noreply.github.com>
2022-09-01 09:49:26 +0530

    Time: 1252 ms (32.51%), Space: 210.6 MB (27.66%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,42 +1,28 @@
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-    unordered_map<int, vector<int>> umap;
-    for(auto x: edges){
-        vector<int>temp = x;
-
-        int u = temp[0];
-        int v = temp[1];
-
-        umap[u].push_back(v);
-        umap[v].push_back(u);
-    }
-//     for(auto &x:mp){
-//         cout<<x.first<<"->";
-//         for(auto i:x.second){
-//             cout<<i<<" ";
-//         }
-//         cout<<endl;
-        
-//     }
-    vector<int> vis(n+1,false);
-    queue<int>q;
-    vis[source]=true;
-    q.push(source);
-    while(!q.empty()){
-        int u=q.front();
-        q.pop();
-        vector<int>temp=umap[u];
-        for(int v:temp){
-            if(vis[v]==false){
-                vis[v]=true;
-                q.push(v);
-            }                    
+        unordered_map<int,vector<int>> umap;
+        for(auto x:edges){
+            vector<int> temp=x;
+            int u=temp[0];
+            int v=temp[1];
+            umap[u].push_back(v);
+            umap[v].push_back(u);
         }
-    }
-    if(vis[destination]){
+        vector<bool> vis(n+1,false);
+        queue<int>q;
+        vis[source]=true;
+        q.push(source);
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            for(int v:umap[u]){
+                if(vis[v]==false){
+                    vis[v]=true;
+                    q.push(v);
+                }
+            }
+        }
         return vis[destination];
-    }
-    return false;
     }
 };
