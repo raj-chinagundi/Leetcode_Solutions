1a706928761e51efdb66dc37197ea7278a41d413
Basavraj Chinagundi <75333151+basavraj-chinagundi@users.noreply.github.com>
2022-09-12 10:06:04 +0530

    Time: 19 ms (12.07%), Space: 10.6 MB (66.86%) - LeetHub

---
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        int c=0;
+        for(auto i:tokens){
+            if(power<i){
+                c++;
+            }
+        }
+        
+        if(tokens.size()==1 && tokens[0]>power || tokens.size()==0 ||c==tokens.size()){
+            return 0;
+        }
+        int n=tokens.size();
+        int s=0;
+        int e=n-1;
+        sort(tokens.begin(),tokens.end());
+        int score=0;
+        int mx=INT_MIN;
+        while(s<=e&&power>=0){
+            if(tokens[s]<=power){
+                power=power-tokens[s];
+                score++;
+                s++;
+            }
+            else if(tokens[s]>power){
+                power=power+tokens[e];
+                score--;
+                e--;
+            }
+            cout<<power<<endl;
+            mx=max(mx,score);
+        }
+        return mx;
+    }
+};
