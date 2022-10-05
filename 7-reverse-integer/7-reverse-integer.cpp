3eccaf8459d3e3068a4f071c5588c84dfedd20bf
Basavraj Chinagundi <75333151+basavraj-chinagundi@users.noreply.github.com>
2022-10-05 14:23:39 +0530

    Time: 3 ms (67.15%), Space: 5.9 MB (75.28%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -3,7 +3,6 @@ public:
     int reverse(int x) {
         int alpha=pow(-2,31);
         int beta=pow(2,31)-1;
-        cout<<beta;
         int temp=x;
         long long int a=0;
         while(x!=0){
