977e0f3128e12d230f4e5385774cfde72ce9a459
Basavraj Chinagundi <75333151+basavraj-chinagundi@users.noreply.github.com>
2022-10-05 17:46:00 +0530

    Time: 23 ms (8.09%), Space: 7.9 MB (7.69%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
-    bool valid(char ch){
-        if((ch>='a' and ch<='z')||(ch>='A' and ch<='Z')||(ch>='0' and ch<='9')){
-            return 1;
+    bool pcheck(string a){
+        int s=0;
+        int e=a.length()-1;
+        while(s<e){
+            if(a[s]!=a[e]){
+                return false;
+            }
+            s++;
+            e--;
         }
-        else{
-            return 0;
-        }
-    }
-    char toLOWER(char m){
-        if((m>='a' and m<='z')||(m>='0' and m<='9')){
-            return m;
-        }
-        else{
-            return m-'A'+'a';
-        }
+        return true;
     }
     bool isPalindrome(string s) {
-    string n;
-    for(int i=0;i<s.length();i++){
-        if(valid(s[i])){
-            n.push_back(toLOWER(s[i]));
+        string alpha="";
+        for(auto c:s){
+            if(isalpha(c)||isdigit(c)){
+                alpha+=tolower(c);
+            }
         }
-    }
-    int st=0;
-    int end=n.length()-1;
-    while(st<=end){
-        if(n[st]!=n[end])return false;
-        else{
-            st++;
-            end--;
-        }
-    }
-        return true;
+        return pcheck(alpha);
     }
 };
