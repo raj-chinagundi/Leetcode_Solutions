c7cbd639cce800e9ed5e1b4d580ba55318f8b66a
Basavraj Chinagundi <75333151+basavraj-chinagundi@users.noreply.github.com>
2022-10-06 21:28:54 +0530

    Time: 25 ms (55.33%), Space: 13.3 MB (54.58%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -2,36 +2,50 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m=matrix.size();
-        int n=matrix[0].size();
-        vector<int> rowmark(m+1,0);
-        vector<int> colmark(n+1,0);
-        //marking row and column where zero exist
+        int n=matrix[0].size();  
+        //3step solution:
+        //bool var
+        bool rowcheck=false;
+        bool colcheck=false;
+        //check col and row zero if any item is zero set true
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+            if(matrix[i][0]==0){
+                colcheck=true;
+                break;
+            }
+        }
+        for(int j=0;j<n;j++){
+            if(matrix[0][j]==0){
+                rowcheck=true;
+                break;
+            }
+        }
+    //check inner matrix mark row zero or column zero as zero if any element found
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
                 if(matrix[i][j]==0){
-                    rowmark[i]=1;
-                    colmark[j]=1;
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
                 }
             }
         }
-        for(auto i:colmark){
-            cout<<i<<" ";
-        }
-        //setup matrix rows a zero
-        for(int i=0;i<m;i++){
-            if(rowmark[i]==1){
-                for(int j=0;j<n;j++){
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0]==0||matrix[0][j]==0){
                     matrix[i][j]=0;
                 }
             }
         }
-        //setup matrix columns a zero
-        for(int i=0;i<n;i++){
-            if(colmark[i]==1){
-                for(int j=0;j<m;j++){
-                    matrix[j][i]=0;
-                }
-            }
+        //now check for row and col booleans
+        if(colcheck){
+             for(int i=0;i<m;i++){
+                matrix[i][0]=0;
+            }           
+        }
+        if(rowcheck){
+             for(int i=0;i<n;i++){
+                matrix[0][i]=0;
+            }           
         }
     }
 };
