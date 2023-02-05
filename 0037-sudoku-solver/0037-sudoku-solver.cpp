95db13844cd9f87a29144b26a23e92aff39f4bd3
Raj Chinagundi <75333151+raj-chinagundi@users.noreply.github.com>
2023-02-05 13:51:00 +0530

    Time: 45 ms (38.20%), Space: 6.4 MB (58.71%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -7,20 +7,25 @@ public:
     bool solve(vector<vector<char>>& board){
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
-                
+                //check if nothing is filled
                 if(board[i][j]=='.'){
                     
                     for(char c='1';c<='9';c++){
+                        //check if that character can be filled
                         if(isValid(i,j,board,c)){
                             board[i][j]=c;
+                            //valid solution found
                             if(solve(board)==true)return true;
+                            //remove that character if it wasnt right solution backtrack
                             else board[i][j]='.';
                         }   
-                    } 
+                    }
+                    //return false for invalid solution
                     return false;   
                 }
             }
         }
+        //everything filled up return true
         return true;
     }
     
