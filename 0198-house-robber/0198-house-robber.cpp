03d29f2ad53f7bb52b833c09816b99fc7e0383da
Raj Chinagundi <75333151+raj-chinagundi@users.noreply.github.com>
2023-02-13 22:40:25 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (55.05%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
-    int solve(int ind,vector<int> &nums,vector<int> &dp){
-        if(ind==0)return nums[ind];
-        if(ind<0)return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        int loot=nums[ind]+solve(ind-2,nums,dp);
-        int noloot=0+solve(ind-1,nums,dp);
-        return dp[ind]=max(loot,noloot);
-    }
+    // int solve(int ind,vector<int> &nums,vector<int> &dp){
+    //     if(ind==0)return nums[ind];
+    //     if(ind<0)return 0;
+    //     if(dp[ind]!=-1) return dp[ind];
+    //     int loot=nums[ind]+solve(ind-2,nums,dp);
+    //     int noloot=0+solve(ind-1,nums,dp);
+    //     return dp[ind]=max(loot,noloot);
+    // }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,-1);
-        return solve(n-1,nums,dp);
+        vector<int> dp(n,0);
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            int loot=nums[i];
+            if(i>1)loot+=dp[i-2];
+            int noloot=0+dp[i-1];
+            dp[i]=max(loot,noloot);
+        }
+        return dp[n-1];
     }
 };
