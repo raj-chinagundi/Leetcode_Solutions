4dafef0164e3bd3c064b7f7deb586c4067701806
Raj Chinagundi <75333151+raj-chinagundi@users.noreply.github.com>
2023-02-14 10:27:04 +0530

    Time: 6 ms (15.38%), Space: 8.1 MB (7.89%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
-    int solve(int ind,vector<int>& nums,vector<int>&dp){
-        if(ind==0)return nums[ind];
-        if(ind<0)return 0;
-        if(dp[ind]!=-1)return dp[ind];
-        int r=nums[ind]+solve(ind-2,nums,dp);
-        int nr=0+solve(ind-1,nums,dp);
-        return dp[ind]=max(r,nr);
+    int solve(vector<int>& nums){
+        int n=nums.size();
+        vector<int> dp(n,0);
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            int r=nums[i];
+            if(i>1)r+=dp[i-2];
+            int nr=0+dp[i-1]; 
+            dp[i]=max(r,nr);
+        }
+
+        return dp[n-1];
     }
     int rob(vector<int>& nums){
         int n=nums.size();
@@ -15,14 +20,13 @@ public:
         }
         vector<int> temp1;
         vector<int> temp2;
-        vector<int> dp1(n,-1);
-        vector<int> dp2(n,-1);
+
         for(int i=0;i<n;i++){
             if(i!=0)temp1.push_back(nums[i]);
             if(i!=n-1)temp2.push_back(nums[i]);
         }
-        int finclude=solve(n-2,temp1,dp1);
-        int linclude=solve(n-2,temp2,dp2);
+        int finclude=solve(temp1);
+        int linclude=solve(temp2);
         return max(finclude,linclude);
     }
 };
