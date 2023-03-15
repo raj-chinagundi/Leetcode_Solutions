321cc38b6f51bb489e0c73f2179495f366e00121
Raj Chinagundi <75333151+raj-chinagundi@users.noreply.github.com>
2023-03-16 01:06:26 +0530

    Time: 229 ms (24.46%), Space: 60.9 MB (23.11%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    bool cycleDFS(int s,vector<int> adj[],vector<int> &vis,vector<int> &pVis){
+    bool cycleDFS(int s,vector<int> adj[],vector<int> &vis,vector<int> &pVis,vector<int> &safeNode){
         vis[s]=1;
         pVis[s]=1;
-        
+        safeNode[s]=0;
         for(auto n:adj[s]){
             if(!vis[n]){
-                if(cycleDFS(n,adj,vis,pVis)) return true;
+                if(cycleDFS(n,adj,vis,pVis,safeNode)) return true;
             }
             else if(pVis[n])return true;
         }
-        
+        safeNode[s]=1;
         pVis[s]=0;
         return false;
     }
@@ -19,6 +19,7 @@ public:
         vector<int> adj[V];
         vector<int> vis(V,0);
         vector<int> pVis(V,0);
+        vector<int> safeNode(V,0);
         for(int i=0;i<V;i++){
             for(int j=0;j<graph[i].size();j++){
                 adj[i].push_back(graph[i][j]);
@@ -26,11 +27,15 @@ public:
         }
         vector<int> ans;
         for(int i=0;i<V;i++){
-                if(cycleDFS(i,adj,vis,pVis)==false){
-                    ans.push_back(i);
+                if(!vis[i]){
+                    cycleDFS(i,adj,vis,pVis,safeNode);
             }
         }
-        sort(ans.begin(),ans.end());
+        for(int i=0;i<safeNode.size();i++){
+            if(safeNode[i]==1){
+                ans.push_back(i);
+            }
+        }
         return ans;
     }
 };
