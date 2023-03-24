092aa789e1b0a3ce4b1eb36ac7c0333a29a8b361
Raj Chinagundi <75333151+raj-chinagundi@users.noreply.github.com>
2023-03-24 10:54:12 +0530

    Time: 60 ms (51.72%), Space: 23.3 MB (32.11%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.cpp
+++ 0934-shortest-bridge/0934-shortest-bridge.cpp
@@ -1,47 +1,47 @@
 class Solution {
 public:
-    void dfs(int sr,int sc,vector<vector<int>> &grid,queue<pair<int,int>> &q){
+    void dfs(int sr,int sc,vector<vector<int>> &grid,queue<pair<pair<int,int>,int>> &q){
         int n=grid.size();
         int m=grid[0].size();
         if(sr<0 or sc <0 or sr>=n or sc>=m or grid[sr][sc]==0 or grid[sr][sc]==2)return;
         
         grid[sr][sc]=2;
-        q.push({sr,sc});
+        q.push({{sr,sc},0});
         
         dfs(sr+1,sc,grid,q);
         dfs(sr-1,sc,grid,q);
         dfs(sr,sc+1,grid,q);
         dfs(sr,sc-1,grid,q);
     }
-    int bfs(vector<vector<int>> &grid,queue<pair<int,int>> &q){
+    int bfs(vector<vector<int>> &grid,queue<pair<pair<int,int>,int>> &q){
         int n=grid.size();
         int m=grid[0].size();
         int dr[]={-1,0,1,0};
         int dc[]={0,-1,0,1};
-        int d=0;
-        int mindis=INT_MAX;
+
+        int mn=INT_MAX;
+        
         while(!q.empty()){
-            int s = q.size();
-            while(s--){
-                int row=q.front().first;
-                int col=q.front().second;
+                int row=q.front().first.first;
+                int col=q.front().first.second;
+                int d =q.front().second;
+                
                 q.pop();
                 for(int i=0;i<4;i++){
                     int nrow=row+dr[i];
                     int ncol=col+dc[i];
 
                     if(nrow>=0 and nrow<n and ncol>=0 and ncol<m and grid[nrow][ncol]==1){
-                        mindis=min(mindis,d);
+                        mn=min(mn,d);//update distance if you find 1 through any bfs from sources
                     }
                     else if(nrow>=0 and nrow<n and ncol>=0 and ncol<m and grid[nrow][ncol]==0){
-                        q.push({nrow, ncol});
+                        q.push({{nrow, ncol},d+1});//keep increasing 1 lvl
                         grid[nrow][ncol]=2;
                     }
                 }
-            }
-            d++;
+
         }
-        return mindis;
+        return mn;
     }
     int shortestBridge(vector<vector<int>>& grid) {
         int n=grid.size();
@@ -57,7 +57,7 @@ public:
                 }
             }
         }
-        queue<pair<int,int>> q;
+        queue<pair<pair<int,int>,int>> q;
         dfs(sr,sc,grid,q);
         return bfs(grid,q);
     }
