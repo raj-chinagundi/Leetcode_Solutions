3aa1bad4212e52d213c5bcdd1765da7ee273188b
Raj Chinagundi <75333151+raj-chinagundi@users.noreply.github.com>
2023-03-26 18:45:10 +0530

    Updated Solution - GfG to GitHub

--- Medium/Distance from the Source (Bellman-Ford Algorithm)/distance-from-the-source-bellman-ford-algorithm.cpp
+++ Medium/Distance from the Source (Bellman-Ford Algorithm)/distance-from-the-source-bellman-ford-algorithm.cpp
@@ -14,30 +14,22 @@ class Solution {
     */
     vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
         // Code here
-		vector<int> dist(V, 1e8);
-		dist[S] = 0;
-		for (int i = 0; i < V - 1; i++) {
-			for (auto it : edges) {
-				int u = it[0];
-				int v = it[1];
-				int wt = it[2];
-				if (dist[u] != 1e8 && dist[u] + wt < dist[v]) {
-					dist[v] = dist[u] + wt;
-				}
-			}
-		}
-		// Nth relaxation to check negative cycle
-		for (auto it : edges) {
-			int u = it[0];
-			int v = it[1];
-			int wt = it[2];
-			if (dist[u] != 1e8 && dist[u] + wt < dist[v]) {
-				return { -1};
-			}
-		}
-
-
-		return dist;
+        vector<int> dist(V,1e8);
+        dist[S]=0;
+        for(int i=0;i<V-1;i++){
+            for(auto x:edges){
+                if(dist[x[0]]!=1e8 and dist[x[0]]+x[2]<dist[x[1]]){
+                    dist[x[1]]=dist[x[0]]+x[2];
+                }
+            }
+        }
+        for(auto x:edges){
+                if(dist[x[0]]!=1e8 and dist[x[0]]+x[2]<dist[x[1]]){
+                    return {-1};
+                }
+        }
+        return dist;
+        
     }
 };
 
