c0348993581f749ad50fa8d2675a4ac8dda7b8f6
Raj Chinagundi <75333151+raj-chinagundi@users.noreply.github.com>
2023-03-26 19:17:23 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.cpp
+++ Floyd Warshall - GFG/floyd-warshall.cpp
@@ -11,35 +11,31 @@ class Solution {
   public:
 	void shortest_distance(vector<vector<int>>&matrix){
 	    // Code here
-		int n = matrix.size();
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (matrix[i][j] == -1) {
-					matrix[i][j] = 1e9;
-				}
-				if (i == j) matrix[i][j] = 0;
-			}
-		}
-
-		for (int k = 0; k < n; k++) {
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					matrix[i][j] = min(matrix[i][j],
-					                   matrix[i][k] + matrix[k][j]);
-				}
-			}
-		}
-
-
-
-
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (matrix[i][j] == 1e9) {
-					matrix[i][j] = -1;
-				}
-			}
-		}
+	    int n=matrix.size();
+         for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==-1){
+                    matrix[i][j]=1e9;
+                }
+                else if(i==j){
+                    matrix[i][j]=0;
+                }
+            }
+        }       
+	    for(int k=0;k<n;k++){
+	        for(int i=0;i<n;i++){
+	            for(int j=0;j<n;j++){
+	                    matrix[i][j]=min(matrix[i][j],matrix[i][k]+matrix[k][j]);
+	            }
+	        }
+	    }
+         for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==1e9){
+                    matrix[i][j]=-1;
+                }
+            }
+        }  
 	}
 };
 
