82ef3cd46e5ca0d04fcbe23375f1cf6534edb37a
Raj Chinagundi <75333151+raj-chinagundi@users.noreply.github.com>
2023-03-26 15:14:02 +0530

    Time: 134 ms (57.45%), Space: 43.4 MB (46.26%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
+    #define pii pair<int,int>
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<pair<int,int>> adj[n+1];
-        for(auto x:times){
-            int u=x[0];
-            int v=x[1];
-            int wt=x[2];
-            
-            adj[u].push_back({v,wt});
-        }
-        
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        pq.push({0,k});
         vector<int> dist(n+1,1e9);
+        for(auto x:times){
+            adj[x[0]].push_back({x[1],x[2]});
+        }
+        priority_queue<pii,vector<pii>,greater<pii>> pq;
+        pq.push({0,k});
         dist[k]=0;
+        
         while(!pq.empty()){
             int node=pq.top().second;
             int dis=pq.top().first;
             pq.pop();
             
             for(auto it:adj[node]){
-                int v=it.first;
-                int wt=it.second;
-                
-                if(dis+wt<dist[v]){
-                    dist[v]=dis+wt;
-                    pq.push({dist[v],v});
+                if(dis+it.second<dist[it.first]){
+                    dist[it.first]=dis+it.second;
+                    pq.push({dist[it.first],it.first});
                 }
             }
         }
-        
         int ans= 0;
         for(int i=1;i<dist.size();i++){
             ans=max(ans,dist[i]);
