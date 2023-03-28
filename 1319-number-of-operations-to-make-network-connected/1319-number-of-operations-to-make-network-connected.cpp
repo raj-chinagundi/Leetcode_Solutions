f944b930b12e3ac24956f75e7e995c5755739261
Raj Chinagundi <75333151+raj-chinagundi@users.noreply.github.com>
2023-03-28 13:00:46 +0530

    Time: 133 ms (78.49%), Space: 42.3 MB (81.60%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,54 @@
+class DisjointSet{
+    vector<int> rank,parent;
+    public:
+        DisjointSet(int n){
+            rank.resize(n+1,0);
+            parent.resize(n+1,0);
+            for(int i=0;i<=n;i++){
+                parent[i]=i;
+            }
+        }
+        
+        int findP(int u){
+            if(u==parent[u]){
+                return u;
+            }
+            return parent[u]=findP(parent[u]);//path compression;
+        }
+        
+        bool unionByRank(int u,int v){
+            int upu=findP(u);
+            int upv=findP(v);
+            
+            if(upu==upv)return false;
+            if(rank[upv]>rank[upu]){
+                parent[upu]=upv;
+            }
+            else if(rank[upu]>rank[upv]){
+                parent[upv]=upu;
+            }            
+            else{
+                parent[upv]=upu;
+                rank[upu]++;
+            }
+            return true;
+        }
+};
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        DisjointSet ds(n-1);
+        for(auto x:connections){
+            int u=x[0];
+            int v=x[1];
+            
+            ds.unionByRank(u,v);
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(ds.findP(i)==i)count++;
+        }
+        int num_edges=connections.size();
+        return (num_edges+1>=n)?count-1:-1;
+    }
+};
