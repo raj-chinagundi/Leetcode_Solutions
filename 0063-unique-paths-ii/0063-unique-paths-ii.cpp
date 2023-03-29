7605ff5f8075287456a28208e9688844a20384ec
Raj Chinagundi <75333151+raj-chinagundi@users.noreply.github.com>
2023-03-29 16:30:25 +0530

    Time: 3 ms (75.51%), Space: 7.8 MB (50.06%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int solve(int i,int j,vector<vector<int>>& obstacleGrid,vector<vector<int>> &dp){
+        if(i>=0 and j>=0 and obstacleGrid[i][j]==1)return 0;//valid cell and obstacle
+        
         if(i==0 and j==0)return 1;
         
-        if(i<0 or j<0 or obstacleGrid[i][j]==1)return 0;
+        if(i<0 or j<0)return 0;
         
         if(dp[i][j]!=-1)return dp[i][j];
         
@@ -14,8 +16,11 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n=obstacleGrid.size();
         int m=obstacleGrid[0].size();
+        // //edge cases
         if(obstacleGrid[n-1][m-1]==1 or obstacleGrid[0][0]==1)return 0;
+        
         if(n==1 and m==1)return 1;
+        
         vector<vector<int>> dp(n,vector<int>(m,-1));
         solve(n-1,m-1,obstacleGrid,dp);
         return dp[n-1][m-1];
