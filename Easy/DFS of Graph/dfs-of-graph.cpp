95d2884230cfbef066dadd0f7c8ae26f0fd960aa
Raj Chinagundi <75333151+raj-chinagundi@users.noreply.github.com>
2023-03-29 23:36:49 +0530

    Updated Solution - GfG to GitHub

--- Easy/DFS of Graph/dfs-of-graph.cpp
+++ Easy/DFS of Graph/dfs-of-graph.cpp
@@ -5,27 +5,26 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    void dfs(int s,vector<int>&vis,vector<int> adj[],vector<int> &ans){
-    	vis[s]=1;
-    	ans.push_back(s);
-    	for(auto it:adj[s]){
-    		if(!vis[it]){
-    			dfs(it,vis,adj,ans);
-    		}
-    	}
-    }
-
     // Function to return a list containing the DFS traversal of the graph.
+    void dfs(int node,vector<int>&vis,vector<int> & ans,vector<int> adj[]){
+        vis[node]=1;
+        ans.push_back(node);
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                dfs(it,vis,ans,adj);
+            }
+        }
+    }
     vector<int> dfsOfGraph(int V, vector<int> adj[]) {
         // Code here
-    	vector<int> ans;
-    	vector<int> vis(V,0);
-    	for(int i=0;i<V;i++){
-    		if(!vis[i]){
-    			dfs(i,vis,adj,ans);
-    		}
-    	}
-    	return ans;
+        vector<int> ans;
+        vector<int> vis(V,0);
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                dfs(i,vis,ans,adj);
+            }
+        }
+        return ans;
     }
 };
 
