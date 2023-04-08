b41995085357e24647d9116088903c3a0b193b98
Raj Chinagundi <75333151+raj-chinagundi@users.noreply.github.com>
2023-04-08 18:29:03 +0530

    Time: 8 ms (12.44%), Space: 7.9 MB (16.29%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -19,25 +19,27 @@ public:
         int n=obstacleGrid[0].size();
         //return f(m-1,n-1,obstacleGrid);
         vector<vector<int>> dp(m,vector<int>(n,0));
-        
+        vector<int> prev(n,0);
         for(int i=0;i<m;i++){
+            vector<int> curr(n,0);
             for(int j=0;j<n;j++){
                 if(obstacleGrid[i][j]==1){
-                    dp[i][j]=0;
+                    curr[j]=0;
                     continue;
                 }
                 if(i==0 and j==0){
-                    dp[i][j]=1;
+                    curr[j]=1;
                     continue;
                 }
                 int up=0;
                 int left=0;
-                if(i>0)up=dp[i-1][j];
-                if(j>0)left=dp[i][j-1];
+                if(i>0)up=prev[j];
+                if(j>0)left=curr[j-1];
                 
-                dp[i][j]=(up+left)%mod;
+                curr[j]=(up+left)%mod;
             }
+            prev=curr;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
