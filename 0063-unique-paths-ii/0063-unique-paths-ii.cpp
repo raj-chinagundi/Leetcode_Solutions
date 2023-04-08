d120ab09b61fbfa69ff71b0500f19ed8098e1c55
Raj Chinagundi <75333151+raj-chinagundi@users.noreply.github.com>
2023-04-08 18:28:01 +0530

    Time: 3 ms (74.96%), Space: 7.7 MB (70.98%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,28 +1,43 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<vector<int>>& obstacleGrid,vector<vector<int>> &dp){
-        if(i>=0 and j>=0 and obstacleGrid[i][j]==1)return 0;//valid cell and obstacle
-        
+    const int mod=2e9;
+    int f(int i,int j,vector<vector<int>>& obstacleGrid){
+        int n=obstacleGrid[0].size();
+        int m=obstacleGrid.size();
         if(i==0 and j==0)return 1;
         
-        if(i<0 or j<0)return 0;
+        if(i<0 or j<0 or i>=m or j>=n or obstacleGrid[i][j]==1){
+            return 0;
+        }
         
-        if(dp[i][j]!=-1)return dp[i][j];
-        
-        int up=solve(i-1,j,obstacleGrid,dp);
-        int left=solve(i,j-1,obstacleGrid,dp);
-        return dp[i][j]=up+left;
+        int up=f(i-1,j,obstacleGrid);
+        int left=f(i,j-1,obstacleGrid);
+        return up+left;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int n=obstacleGrid.size();
-        int m=obstacleGrid[0].size();
-        // //edge cases
-        if(obstacleGrid[n-1][m-1]==1 or obstacleGrid[0][0]==1)return 0;
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        //return f(m-1,n-1,obstacleGrid);
+        vector<vector<int>> dp(m,vector<int>(n,0));
         
-        if(n==1 and m==1)return 1;
-        
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        solve(n-1,m-1,obstacleGrid,dp);
-        return dp[n-1][m-1];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
+                    continue;
+                }
+                if(i==0 and j==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+                int up=0;
+                int left=0;
+                if(i>0)up=dp[i-1][j];
+                if(j>0)left=dp[i][j-1];
+                
+                dp[i][j]=(up+left)%mod;
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
