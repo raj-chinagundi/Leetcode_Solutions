5d80a8fa5b8fe6eb0e26d48295fb6c0de99fdbdf
Raj Chinagundi <75333151+raj-chinagundi@users.noreply.github.com>
2023-04-09 13:47:31 +0530

    Time: 13 ms (26.86%), Space: 10.5 MB (5.82%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -14,22 +14,24 @@ public:
         int n=grid[0].size();
         // return f(m-1,n-1,grid);
         vector<vector<int>> dp(m,vector<int>(n,0));
-        dp[0][0]=grid[0][0];
+        vector<int> prev(n,0);
         for(int i=0;i<m;i++){
+            vector<int> curr(n,0);
             for(int j=0;j<n;j++){
                 if(i==0 and j==0){
-                    dp[i][j]=grid[i][j];
+                    curr[j]=grid[i][j];
                     continue;
                 }
                 int up=grid[i][j];
                 int left=grid[i][j];
-                if(i>0)up+=dp[i-1][j];
+                if(i>0)up+=prev[j];
                 else up+=1e9;
-                if(j>0)left+=dp[i][j-1];
+                if(j>0)left+=curr[j-1];
                 else left+=1e9;
-                dp[i][j]=min(up,left);
+                curr[j]=min(up,left);
             }
+            prev=curr;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
