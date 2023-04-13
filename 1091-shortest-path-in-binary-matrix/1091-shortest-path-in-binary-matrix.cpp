581c64f0297aa13cb7668a2a6000838bb149f3ad
Raj Chinagundi <75333151+raj-chinagundi@users.noreply.github.com>
2023-04-13 20:40:55 +0530

    Time: 149 ms (21.05%), Space: 31.6 MB (23.11%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
-    #define ppiii pair<int,pair<int,int>>
+    #define pii pair<int,pair<int,int>>
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>> dist(n,vector<int>(m,1e9));
-        
         if(grid[0][0]==1)return -1;
         if(n-1==0 and m-1==0)return 1;
         
-        priority_queue<ppiii,vector<ppiii>,greater<ppiii>> q;
-        q.push({1,{0,0}});
+        vector<vector<int>> dist(n,vector<int>(m,1e9));
+        set<pii> st;
+        st.insert({1,{0,0}});
         dist[0][0]=1;
+        
         int dr[]={-1,0,1,0,-1,1,1,-1};
         int dc[]={0,-1,0,1,1,-1,1,-1};
         
-        while(!q.empty()){
-            auto it=q.top();
+        while(!st.empty()){
+            auto it=*st.begin();
             int row=it.second.first;
             int col=it.second.second;
-            int d=it.first;
-            q.pop();
+            int dis=it.first;
+            st.erase(it);
             
             for(int i=0;i<8;i++){
-                int nrow=dr[i]+row;
-                int ncol=dc[i]+col;
-                if(nrow>=0 and ncol>=0 and nrow<n and ncol<m and grid[nrow][ncol]==0 and d+1<dist[nrow][ncol]){
-                        dist[nrow][ncol]=d+1;
+                int nrow=row+dr[i];
+                int ncol=col+dc[i];
+                
+                if(nrow>=0 and ncol>=0 and nrow<n and ncol<m and grid[nrow][ncol]==0){
+                    if(dis+1<dist[nrow][ncol]){
                         if(nrow==n-1 and ncol==m-1){
-                            return d+1;
+                            return dis+1;
                         }
-                        q.push({d+1,{nrow,ncol}});
+                        if(dist[nrow][ncol]!=1e9){
+                            st.erase({dist[nrow][ncol],{nrow,ncol}});
+                        }
+                        dist[nrow][ncol]=dis+1;
+                        st.insert({dist[nrow][ncol],{nrow,ncol}});
+                    }
                 }
             }
-            
         }
         return -1;
     }
