a917c30c66390dd0ff102bb2b64c84ddb1a1c382
Raj Chinagundi <75333151+raj-chinagundi@users.noreply.github.com>
2023-04-13 12:02:39 +0530

    Updated Solution - GfG to GitHub

--- Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.cpp
+++ Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.cpp
@@ -6,32 +6,24 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in an undirected graph.
-    bool bfs(int node,vector<int> &vis,vector<int> adj[]){
-        queue<pair<int,int>> q;
-        q.push({node,-1});
+    bool dfs(int node,int parent,vector<int> &vis,vector<int> adj[]){
         vis[node]=1;
-        while(!q.empty()){
-            int node=q.front().first;
-            int parent=q.front().second;
-            q.pop();
-            for(auto it:adj[node]){
-                if(!vis[it]){
-                    vis[it]=1;
-                    q.push({it,node});
-                }
-                else if(it!=parent){
-                    return true;
-                }
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                if(dfs(it,node,vis,adj))return true;
+            }
+            else if(it!=parent){
+                return true;
             }
         }
-        return false;        
+        return false;
     }
     bool isCycle(int V, vector<int> adj[]) {
         // Code here
         vector<int> vis(V,0);
         for(int i=0;i<V;i++){
             if(!vis[i]){
-                if(bfs(i,vis,adj))return true;
+                if(dfs(i,-1,vis,adj))return true;
             }
         }
         return false;
