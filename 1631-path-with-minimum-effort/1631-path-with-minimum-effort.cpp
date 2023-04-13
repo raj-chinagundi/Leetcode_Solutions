37964e12c6eeb92bf7dc0be110d909b1033c8804
Raj Chinagundi <75333151+raj-chinagundi@users.noreply.github.com>
2023-04-13 22:57:06 +0530

    Time: 203 ms (35.35%), Space: 37.2 MB (20.54%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,42 +1,43 @@
 class Solution {
 public:
-    #define ppi pair<int,pair<int,int>> 
+    #define pii pair<int,pair<int,int>>
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n=heights.size();
         int m=heights[0].size();
         
-        vector<vector<int>> dist(n,vector<int>(m,1e9));
-        priority_queue<ppi,vector<ppi>,greater<ppi>> pq;
-        pq.push({0,{0,0}});
-        dist[0][0]=0;
-        
         int dr[]={-1,0,1,0};
         int dc[]={0,-1,0,1};
-
-        while(!pq.empty()){
-            int diff=pq.top().first;
-            int row=pq.top().second.first;
-            int col=pq.top().second.second;
-            pq.pop();
+        
+        set<pii> st;
+        vector<vector<int>> dist(n,vector<int>(m,1e9));
+        //diff of effort in path,bfs point
+        st.insert({0,{0,0}});
+        dist[0][0]=0;
+        while(!st.empty()){
+            auto it=*st.begin();
+            int row=it.second.first;
+            int col=it.second.second;
+            int dis=it.first;
+            st.erase(it);
             
-            if(row==n-1 and col==m-1){
-                return diff;
-            }
-
             for(int i=0;i<4;i++){
-                int nrow=row+dr[i];
-                int ncol=col+dc[i];
+                int nrow=dr[i]+row;
+                int ncol=dc[i]+col;
                 
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
-                    //abhi tak hitna bhi traverse kiya uss path mai uska max effort store krunga
-                    int effort=max(abs(heights[row][col]-heights[nrow][ncol]),diff);
-                    if(effort<dist[nrow][ncol]){
-                        dist[nrow][ncol]=effort;
-                        pq.push({dist[nrow][ncol],{nrow,ncol}});                        
+                if(nrow>=0 and ncol>=0 and nrow<n and ncol<m){
+                    int effort=abs(heights[nrow][ncol]-heights[row][col]);
+                    int newDist = max(dist[row][col], effort); // calculate new distance
+                    if(newDist < dist[nrow][ncol]){
+                        if(dist[nrow][ncol]!=1e9){
+                            st.erase({dist[nrow][ncol],{nrow,ncol}});
+                        }
+                        dist[nrow][ncol]=newDist; // update distance
+                        st.insert({newDist,{nrow,ncol}});
                     }
                 }
-            }    
+            }
         }
-        return -1;
+        return dist[n-1][m-1];
+        
     }
 };
