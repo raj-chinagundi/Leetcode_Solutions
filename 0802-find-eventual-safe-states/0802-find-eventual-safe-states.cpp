e609e47b41440c316ce1ca6188f00fffd80351be
Raj Chinagundi <75333151+raj-chinagundi@users.noreply.github.com>
2023-04-13 19:45:47 +0530

    Time: 217 ms (33.74%), Space: 60.1 MB (28.53%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-    bool dfs(int s,vector<int>&vis,vector<int>&pVis,vector<int> adj[]){
-        vis[s]=1;
-        pVis[s]=1;
-        for(auto it:adj[s]){
+    bool dfs(int node,vector<int>&vis,vector<int>&pVis,vector<int> adj[]){
+        vis[node]=1;
+        pVis[node]=1;
+        for(auto it:adj[node]){
             if(!vis[it]){
                 if(dfs(it,vis,pVis,adj))return true;
             }
-            else if(pVis[it])return true;
+            else if(pVis[it]){
+                return true;
+            }
         }
-        pVis[s]=0;
-        
+        pVis[node]=0;
         return false;
     }
+    
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
         vector<int> adj[n];
         for(int i=0;i<n;i++){
-            for(auto x:graph[i]){
-                adj[i].push_back(x);
+            for(auto it:graph[i]){
+                adj[i].push_back(it);
             }
         }
-        vector<int> ans;
         vector<int> vis(n,0);
         vector<int> pVis(n,0);
+        vector<int> ans; 
         for(int i=0;i<n;i++){
            if(dfs(i,vis,pVis,adj)==false)ans.push_back(i);
         }
