5a34b81f8951804b2f6878400db3523a9b1f3620
Raj Chinagundi <75333151+raj-chinagundi@users.noreply.github.com>
2023-04-13 13:42:51 +0530

    Time: 32 ms (22.23%), Space: 18.3 MB (5.01%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    bool dfs(int p,int pcolor,vector<int> &vis,vector<int> &color,vector<int> adj[]){
-        vis[p]=1;
-        for(auto it:adj[p]){
+    bool dfs(int node,int color,vector<int> &pcolor,vector<int> &vis,vector<int> adj[]){
+        vis[node]=1;
+        for(auto it:adj[node]){
             if(!vis[it]){
-                color[it]=!pcolor;
-                if(dfs(it,color[it],vis,color,adj)) return true;
+                pcolor[it]=!color;
+                if(dfs(it,!color,pcolor,vis,adj))return true;
             }
-            else if(color[it]==color[p])return true;
+            else if(pcolor[it]==color){
+                return true;
+            }
         }
         return false;
     }
     bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
-        int m=graph[0].size();
-        vector<int> adj[n];
-        for(int i=0;i<graph.size();i++){
-            for(auto x:graph[i]){
-                adj[i].push_back(x);
-                adj[x].push_back(i);
+        int V=graph.size();
+        vector<int> adj[V];
+        for(int i=0;i<V;i++){
+            for(auto it:graph[i]){
+                adj[i].push_back(it);
+                adj[it].push_back(i);
             }
         }
-        vector<int> color(n,0);
-        vector<int> vis(n,0);
-        for(int i=0;i<n;i++){
+        
+        vector<int> vis(V,0);
+        vector<int> pcolor(V,0);
+        int initial_color=0;
+        for(int i=0;i<V;i++){
             if(!vis[i]){
-                if(dfs(i,0,vis,color,adj))return false;
+                if(dfs(i,initial_color,pcolor,vis,adj)==true)return false;
             }
         }
         return true;
