3b0e0f9c52f0b331836757b3c858ca8524b653b7
Raj Chinagundi <75333151+raj-chinagundi@users.noreply.github.com>
2023-05-01 14:45:17 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (82.11%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto &c:s){
+            if(c=='(' || c=='{' || c=='['){
+                st.push(c);
+            }
+            else if(c==')'){
+                if(!st.empty() && st.top()=='('){
+                    st.pop();
+                }
+                else return false;
+            }
+            else if(c=='}'){
+                if(!st.empty() && st.top()=='{'){
+                    st.pop();
+                }
+                else return false;
+            }
+            else if(c==']'){
+                if(!st.empty() && st.top()=='['){
+                    st.pop();
+                }
+                else return false;
+            }
+        }
+        return st.empty();
+    }
+};
